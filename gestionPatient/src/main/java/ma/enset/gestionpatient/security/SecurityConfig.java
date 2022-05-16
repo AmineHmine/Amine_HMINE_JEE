fe49bbb27b34301392024532fccdffb407a344a5
Amine Hmine <aminehmine1@gmail.com>
2022-05-16 03:12:49 +0100

    adding security config using jdbc

--- gestionPatient/src/main/java/ma/enset/gestionpatient/security/SecurityConfig.java
+++ gestionPatient/src/main/java/ma/enset/gestionpatient/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ma.enset.gestionpatient.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,17 +10,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = passwordEncoder();
-        String encodedPwd = passwordEncoder.encode("1234");
-        System.out.println(encodedPwd);
-        auth.inMemoryAuthentication().withUser("user1").password(encodedPwd).roles("USER");
-        auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1111")).roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("2345")).roles("USER","ADMIN");
+//        PasswordEncoder passwordEncoder = passwordEncoder();
+//        String encodedPwd = passwordEncoder.encode("1234");
+//        System.out.println(encodedPwd);
+//        auth.inMemoryAuthentication().withUser("user1").password(encodedPwd).roles("USER");
+//        auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1111")).roles("USER");
+//        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("2345")).roles("USER","ADMIN");
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal,password as credentials, active from users where username=?")
+                .authoritiesByUsernameQuery("select username as principal,role as role from users_roles where username=?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
