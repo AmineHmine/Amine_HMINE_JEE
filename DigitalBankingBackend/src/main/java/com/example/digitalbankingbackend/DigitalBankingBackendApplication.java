e561f7816477f2e33890c978ae7d32e36c741ccd
Amine Hmine <aminehmine1@gmail.com>
2022-05-21 10:47:07 +0100

    Digital-Banking-Backend

---
+++ DigitalBankingBackend/src/main/java/com/example/digitalbankingbackend/DigitalBankingBackendApplication.java
@@ -0,0 +1,21 @@
+package com.example.digitalbankingbackend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DigitalBankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingBackendApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner start(){
+        return args -> {
+            System.out.println("success");
+        };
+    }
+}
